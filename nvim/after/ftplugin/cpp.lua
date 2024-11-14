44e67e1e2f6416186f74b95a4a953efc0e656c5f
NotTHDAnh <99874931+NotTHDAnh@users.noreply.github.com>
2024-11-14 10:20:11 +0700

    Add files via upload

---
+++ nvim/after/ftplugin/cpp.lua
@@ -0,0 +1,1 @@
+vim.keymap.set('n','<C-B>',"<cmd>w!<Cr> <cmd>FloatermNew --position=topright --autoclose=0 g++ % -o a && a <CR>")
---
+++ nvim/lua/core/keymaps.lua
@@ -0,0 +1,15 @@
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.foldlevel = 99
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.textwidth = 120
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.fileformat = unix
+vim.keymap.set('n','<C-Y>',"<cmd>%y+<Cr>")
+
